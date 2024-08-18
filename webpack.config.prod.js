099631baaf01fe9d99d248512eb0f8b598c44da6
Julien Milo <julienmilo369@gmail.com>
2024-08-18 14:38:15 +0200

    webpack fix

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -6,24 +6,30 @@ const CopyPlugin = require('copy-webpack-plugin');
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
+    // Génère le fichier index.html
     new HtmlWebpackPlugin({
       template: './index.html',
       filename: 'index.html',
     }),
+    // Génère le fichier contact.html
+    new HtmlWebpackPlugin({
+      template: './contact.html',
+      filename: 'contact.html',
+    }),
+    // Copie les ressources statiques
     new CopyPlugin({
       patterns: [
         { from: 'img', to: 'img' },
         { from: 'css', to: 'css' },
         { from: 'js/vendor', to: 'js/vendor' },
-        { from: 'js', to: 'js' },
+        { from: 'js', to: 'js' }, // Copier les fichiers JS s'ils ne sont pas gérés par Webpack
         { from: 'icon.svg', to: 'icon.svg' },
         { from: 'favicon.ico', to: 'favicon.ico' },
         { from: 'robots.txt', to: 'robots.txt' },
         { from: 'icon.png', to: 'icon.png' },
         { from: '404.html', to: '404.html' },
-        { from: 'contact.html', to: 'contact.html' },
         { from: 'site.webmanifest', to: 'site.webmanifest' },
-        { from: 'fonts', to: 'fonts' },
+        { from: 'fonts', to: 'fonts' }, // Copier les polices
       ],
     }),
   ],
